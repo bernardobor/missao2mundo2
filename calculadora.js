3e8f02cb087b3ad1b35c1be5b44ecdc6c4386884
Bernardo Ramos <bernardo_bor@hotmail.com>
2024-04-10 20:04:54 -0300

    final da primeira aula

---
+++ calculadora.js
@@ -0,0 +1,14 @@
+function somar(a,b){return a+b}
+function subtrair(a,b){return a-b}
+function multiplicar(a,b){return a*b}
+
+function dividir(a, b){
+    if(b == 0){
+        return "Não é possível dividir por 0"
+    }
+    return a/b
+}
+
+function exponenciar(a, b){
+    return a**b
+}
